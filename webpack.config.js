823d1173dc206ace6813b87f858d316a361fc902
atgrafic <troczynska.anna@gmail.com>
2021-06-11 18:59:25 +0200

    change webpac.cnfig

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
 
     output: {
         path: path.resolve(__dirname, "dist"),
-        publicPath: "/",
+        // publicPath: "/",
         filename: "[name].[contenthash].bundle.js",
     },
     devServer: {
