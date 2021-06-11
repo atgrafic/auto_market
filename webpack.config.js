0c1c187d61bb90ca7d7df26468171d9fe5dbaddf
atgrafic <troczynska.anna@gmail.com>
2021-06-11 19:06:10 +0200

    chanfe webpackconfig

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ module.exports = {
             template: "./src/index.html",
         }),
 
-        new CopyPlugin([{ from: "src/assets/image", to: "./image" }]),
+        new CopyPlugin([{ from: "src/assets/image", to: "image" }]),
         // new CleanWebpackPlugin(),
     ],
     module: {
