f45050ca38738dabab106383f17a6348bdcf0bbe
atgrafic <troczynska.anna@gmail.com>
2021-06-11 19:07:56 +0200

    change webpack

--- webpack.config.js
+++ webpack.config.js
@@ -51,7 +51,7 @@ module.exports = {
                 use: {
                     loader: "file-loader",
                     options: {
-                        outputPath: "./image",
+                        outputPath: "image",
                         name: "[name].[ext]",
                     },
                 },
